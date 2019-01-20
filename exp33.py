9ef6f4444d74bdf15bcfc199b69cc9e6bafc1145
HarishAVM <46394904+HarishAVM@users.noreply.github.com>
2019-01-20 17:48:52 +0530

    Update exp33.py

--- exp33.py
+++ exp33.py
@@ -1,5 +1,5 @@
-ha=raw_input()
-if(ha=='A' or ha=='a' or ha=='E' or ha=='e' or ha=='I' or ha=='i' or ha=='O' or ha=='o' or ha=='U' or ha=='u'):
+z=raw_input()
+if(z=='A' or z=='a' or z=='E' or z=='e' or z=='I' or z=='i' or z=='O' or z=='o' or z=='U' or z=='u'):
  print("Vowel")
 else:
  print("Consonant")
